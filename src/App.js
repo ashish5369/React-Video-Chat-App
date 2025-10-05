41413c2f399dc719da14dec2cd2488631a99fc81
Ashish Singh <89240148+ashish5369@users.noreply.github.com>
2025-10-05 17:54:28 +0000

    base

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React Video Chat App</h1>
+        <p>
+          Start building your video chat application here!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
